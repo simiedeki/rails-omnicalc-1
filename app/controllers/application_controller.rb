d743626d26087e700bf67ff15eeb88b6b7c07a1c
simiedeki <164128583+simiedeki@users.noreply.github.com>
2024-04-17 00:39:05 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,48 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  def home
+  end
+  
+  def squarenew
+  end
+  
+  def paymentnew
+  end
+  
+  def paymentresults
+    @user_apr = params.fetch("user_apr").to_f # Convert APR from percentage to a decimal
+    @user_years = params.fetch("user_years").to_f
+    @user_pv = params.fetch("user_pv").to_f
+    
+    @monthly_interest_rate = @user_apr / 1200
+    @total_payments = @user_years * 12
+    unformatted_payment = (@monthly_interest_rate * @user_pv) / (1 - (1 + @monthly_interest_rate)**-@total_payments)
+  
+    @monthly_payment = unformatted_payment.to_fs(:currency)
+  end
+  
+  def randomnew
+  end
+  
+  def randomresults 
+    @user_min = params.fetch("user_min").to_f
+   @user_max = params.fetch("user_max").to_f
+    @random_number = rand(@user_min..@user_max)
+  end
+  
+
+  def squarerootnew
+  end
+  
+  def squareresults
+    @num = params.fetch("number").to_f
+    @squared = @num*@num
+  end
+    
+  
+  def squarerootresults
+    @num = params.fetch("number").to_f
+    @squareroot = (@num)**0.5
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+
+
 Rails.application.routes.draw do
-  
+    root 'application#squarenew'
+    get '/', to: redirect('/square/new')
+    get '/square/new', to: 'application#squarenew'
+    get '/square/results', to: 'application#squareresults'
+    get '/payment/new', to: 'application#paymentnew'
+    get '/payment/results', to: 'application#paymentresults'
+    get '/random/new', to: 'application#randomnew'
+    get '/random/results', to: 'application#randomresults'
+    get '/square_root/new', to: 'application#squarerootnew'
+    get '/square_root/results', to: 'application#squarerootresults'
 end
